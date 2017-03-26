839610832292894cb73261c81b3645becdbd0bef
caiqifang <cai2053@gmail.com>
2017-03-25 22:16:50 -0400

    try

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,23 +1,37 @@
 window.onload = function(){
     // create a wrapper around native canvas element
     var canvas = new fabric.Canvas('main');
-    var SelfReloadJSON = require('self-reload-json');
+    // var SelfReloadJSON = require('self-reload-json');
 
     // load json files
     var json_set = new Object();
+    var json_data = new Object();
 
-    var json_data = new SelfReloadJSON("../static/data.json");
+    // init
+    json_data.x = 50;
+    json_data.y = 50;
 
     // create a rectangle object - > your position
     var rect = new fabric.Rect({
-        left: 100,
-        top: 100,
+        left: json_data.x,
+        top: json_data.y,
         fill: 'red',
         width: 10,
         height: 10
     });
 
     // create anchors
+    var tri1 = new fabric.Triangle({
+        width: 10, height: 10, fill: 'blue', left: 50, top: 50
+    });
+
+    var tri2 = new fabric.Triangle({
+        width: 10, height: 10, fill: 'blue', left: 750, top: 50
+    });
+
+    var tri3 = new fabric.Triangle({
+        width: 10, height: 10, fill: 'blue', left: 50, top: 550
+    });
 
 
 
@@ -29,12 +43,18 @@ window.onload = function(){
 
     // "add"  elements onto canvas
     canvas.add(rect);
+    canvas.add(tri1);
+    canvas.add(tri2);
+    canvas.add(tri3);
 
     // refresh
-    setInterval(draw, 5000);
+    setInterval(draw, 3000);
 
     // drawing function
     function draw(){
+        // load
+        loadJSON("../static/data.json", load_data);
+
         if(json_data.x > -1){
             // reset positions
             rect.set({ left: json_data.x, top: json_data.y });
@@ -45,24 +65,22 @@ window.onload = function(){
     // loading
     function loadJSON(file, callback){
       var xobj = new XMLHttpRequest();
-      xobj.overrideMimeType("application/json");
-      xobj.open('GET', file, true);
+      //xobj.overrideMimeType("application/json");
       xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
+        if (this.readyState == 4 && this.status == "200") {
         // Required use of an anonymous callback as .open will NOT return a
         // //value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
+            callback(this);
         }
       };
-      xobj.send(null);
+      xobj.open("GET", file, true);
+      xobj.send();
     }
 
-    function load(file, result){
-        loadJSON(file, function(response){
-            var json_real = JSON.parse(response);
-            result.x = json_real.x;
-            result.y = json_real.y;
-        });
+    function load_data(obj){
+        var json_real = JSON.parse(obj.responseText);
+        json_data.x = json_real.x;
+        json_data.y = json_real.y;
     }
 
 }
