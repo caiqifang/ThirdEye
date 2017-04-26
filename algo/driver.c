09afb6a5bba80e30d01090038d0734bb71a7a0a5
caiqifang <cai2053@gmail.com>
2017-04-26 16:34:20 -0400

    new function in driver.c

--- algo/driver.c
+++ algo/driver.c
@@ -8,7 +8,7 @@
 #include <netdb.h>
 
 #define MAX_SIZE 1024
-#define MIN_SIZE 256
+#define MIN_SIZE 32
 
 void error(const char *msg)
 {
@@ -20,8 +20,19 @@ void error(const char *msg)
 // TODO: collecting DW information and put into buffer
 void pull_info(char *buffer){
     memset(buffer, 0, MAX_SIZE);
-    // change this test
+    // TODO: rewrite this function
+    // TODO: remove this, this is for test purpose
     fgets(buffer,255,stdin);
+}
+
+// TODO: send out alert to tags
+// server return 0 -> no alert
+// server return 1 -> has alert
+void send_alert(char* reply){
+    size_t str_len = MIN_SIZE;
+    // TODO: implement this function
+    // if alert is true, send alert
+    // else ignore reply
 }
 
 void send_info(char *msg, int argc, char *argv[], char *reply){
@@ -50,13 +61,21 @@ void send_info(char *msg, int argc, char *argv[], char *reply){
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
+    /* char* msg contains the location information
+     * here we send the msg to calculation in python */
     n = write(sockfd, msg, MAX_SIZE);
+
     if (n < 0)
         error("ERROR writing to socket");
-    n = read(sockfd, reply, 255);
+
+    /* we have reply from server, if alert occur, send alert to tags */
+    n = read(sockfd, reply, MIN_SIZE -1);
     if (n < 0)
         error("ERROR reading from socket");
     printf("Return message: %s\n", reply);
+    /* we have reply from server, if alert occur, send alert to tags */
+    send_alert(reply);
+
     close(sockfd);
     return;
 }
@@ -66,9 +85,9 @@ int main(int argc, char *argv[])
 {
     char *buffer, *reply;
     buffer = (char *) malloc(MAX_SIZE);
-    reply = (char *) malloc(256);
+    reply = (char *) malloc(MIN_SIZE);
     while(1){
-        pull_info(buffer);
+        pull_info(buffer); // collect information
         send_info(buffer, argc, argv, reply);
     }
     free(buffer);
