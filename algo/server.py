5b0280dfe0592187a8e9086c951d35df34ddf9a6
caiqifang <cai2053@gmail.com>
2017-04-27 16:05:14 -0400

    finish

--- algo/server.py
+++ algo/server.py
@@ -17,21 +17,19 @@ def sign(v):
         return 1
     return -1
 
-def calc_error(num_device, num_tag, dmatrix, point, i, obj):
+def calc_error(distance, point, key, obj):
     e = 0
-    for d in range(num_tag, num_tag + num_device):
-        cal_dis = calc_dis(obj['anchors_list'][d]['left'],
-                         obj['anchors_list'][d]['top'], point[0], point[1])
-        e = e + (cal_dis - dmatrix[i][d])**2
+    for beacon in distance[key].keys():
+        cal_dis = calc_dis(obj['anchors_list'][beacon]['left'],
+                         obj['anchors_list'][beacon]['top'], point[0], point[1])
+        e = e + (cal_dis - distance[key][beacon])**2
     return e
 
-def find_point(result, status, point, i):
+def find_point(result, status, point, key):
     p = point
-    num_device = result['num_device']
-    num_tag = result['num_tag']
-    matrix = result['distance']
-    error = calc_error(num_device, num_tag, matrix, p, i)
-    step = 10
+    distance = result['distance']
+    error = calc_error(distance, p, key, status['setting'])
+    step = 10.0
     cnt = 0
     while (error > 1):
         # choose x y to improve
@@ -42,7 +40,7 @@ def find_point(result, status, point, i):
             vector = (0, step)
         point_p = (p[0] + vector[0], p[1] + vector[1])
         # calculate gradiant
-        err_change = (calc_error(num_device, num_tag, matrix, point_p, i) \
+        err_change = (calc_error(distance, point_p, key, status['setting']) \
                             - error) / step
         # choose step size
         step = math.sqrt(abs(err_change))
@@ -52,31 +50,32 @@ def find_point(result, status, point, i):
         else:
             p = (p[0], p[1] - step*sign(err_change))
         # update improve
-        error = calc_error(num_device, num_tag, matrix, p, i)
+        error = calc_error(distance, p, key, status['setting'])
         cnt = cnt +1
         if cnt > 100:
             break
     return p
 
 def locate(result, status):
-    num_device = result['num_device']
-    num_tag = result['num_tag']
-    point = []
+    points = {}
+    point = ()
     if status['last_point'] == None:
         #first start
         corner = (status['setting']['bound_left'], status['setting']['bound_top'])
         max_bound =(status['setting']['max_width'], status['setting']['max_height'])
-        for i in num_tag:
-            point.append((random.random()*max_bound[0] + corner[0],
+        point = ((random.random()*max_bound[0] + corner[0],
                             random.random()*max_bound[1] + corner[1]))
     else:
         point = status['last_point']
 
-    for i in num_tag:
-        point[i] = find_point(result, status, point[i], i)
+    for key in result['distance'].keys():
+        ret = find_point(result, status, point, key)
+        points[key] = {}
+        points[key]['x'] = ret[0]
+        points[key]['y'] = ret[1]
+        status['last_point'] = ret
 
-    status['last_point'] = point
-    return point
+    return points
 
 # ============ FILE IO
 def load_json(path):
@@ -191,7 +190,7 @@ def main():
                         # write to data.json
                         write_json(data_path, ret)
                         # Detect Alert
-                        msg = isInArea(status, ret):
+                        msg = isInArea(status, ret)
                     c.send(msg)
                     client_msg = c.recv(1024)
             except:
